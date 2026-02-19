84aa9a16c29e5b768ab76bc7daeb3f858e517c09
Arthur Papp <59942836+arthurPapp@users.noreply.github.com>
2026-02-19 15:06:50 +0000

    ajuste pritter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto criado com o curso.dev para aperfeiçoar minha experiencia dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Camila e Raphael eu amo vcs, vamos estudar mais para garantir o melhor para vcs 😍</h1>
+function Home() {
+  return (
+    <h1>
+      Camila e Raphael eu amo vcs, vamos estudar mais para garantir o melhor
+      para vcs 😍
+    </h1>
+  );
 }
 
 export default Home;
