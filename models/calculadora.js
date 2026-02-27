9bf2c28b39222e66b52390704c3eeaea037e1421
Arthur Papp <59942836+arthurPapp@users.noreply.github.com>
2026-02-27 14:55:08 +0000

    testes para falar de testes

--- models/calculadora.js
+++ models/calculadora.js
@@ -1,4 +1,6 @@
 function somar(n1, n2) {
+
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') return 'ERROR';
     return n1 + n2;
 }
 
--- testes/calculadora.test.js
+++ testes/calculadora.test.js
@@ -9,3 +9,20 @@ test('somar 5 + 100 deveria retornar ', () => {
     const resultado = calculadora.somar(5, 100);
     expect(resultado).toBe(105)
 })
+
+test('somar banana  + 100 deveria retornar erro ', () => {
+    const resultado = calculadora.somar('banana', 100);
+    expect(resultado).toBe('ERROR')
+})
+
+
+test('somar " "  + " " deveria retornar erro ', () => {
+    const resultado = calculadora.somar("", 100);
+    expect(resultado).toBe('ERROR')
+})
+
+
+test('somar 1.5  + 15 deveria retornar 2', () => {
+    const resultado = calculadora.somar(1.5, 15);
+    expect(resultado).toBe(16.5)
+})
