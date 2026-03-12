a3eeee8a6d87ea49afa93e55d1b2db8f2b954743
Arthur Papp <59942836+arthurPapp@users.noreply.github.com>
2026-03-12 14:07:08 +0000

    add

---
+++ infra/database.js
@@ -0,0 +1,23 @@
+import { Client } from 'pg';
+
+async function query(queryObject) {
+
+    const client = new Client({
+        host: 'localhost',
+        port: 5432,
+        user: 'postgres',
+        database: 'postgres',
+        password: '@Materuzo199'
+    });
+
+    await client.connect();
+    const result = await client.query(queryObject)
+    await client.end();
+
+    return result;
+}
+
+
+export default {
+    query: query
+};
