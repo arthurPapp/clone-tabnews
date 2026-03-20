64f2c1c12fc20fb624e37fe2e57a76320ebab298
Arthur Papp <59942836+arthurPapp@users.noreply.github.com>
2026-03-20 18:26:04 +0000

    status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,11 @@
 import database from 'infra/database.js';
 
 async function status(request, response) {
-    const result = await database.query('SELECT 1+1 as sum');
+    const updatedAt = new Date().toISOString();
 
-    console.log(result.rows);
-
-    response.status(200).json({ message: "olá dev" });
+    response.status(200).json({
+        updated_at: updatedAt
+    });
 }
 
 export default status;
