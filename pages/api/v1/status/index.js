47732c17a2db57fd0f6ae15c439777fa8b51dd81
Arthur Papp <59942836+arthurPapp@users.noreply.github.com>
2026-03-23 20:23:31 +0000

    add version

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,20 @@
 import database from 'infra/database.js';
+import { version } from 'react';
 
 async function status(request, response) {
+
+
     const updatedAt = new Date().toISOString();
+    const databaseVersionResult = await database.query('SHOW server_version;');
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
     response.status(200).json({
-        updated_at: updatedAt
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                version: databaseVersionValue,
+            }
+        }
     });
 }
 
