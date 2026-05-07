616108e0979d692d4e4e0fb58c5f68b7158207e8
Arthur Papp <59942836+arthurPapp@users.noreply.github.com>
2026-05-07 18:09:43 +0000

    add

--- testes/integration/api/v1/migrations/get.test.js
+++ testes/integration/api/v1/migrations/get.test.js
@@ -1,9 +1,23 @@
-test('GET to  /api/v1/status should return 200', async () => {
+import database from 'infra/database.js'
+
+beforeAll(cleanDatabase)
+
+async function cleanDatabase() {
+    await database.query("drop schema public cascade; create schema public;")
+}
+
+test('GET to  /api/v1/migrations should return 200', async () => {
     const response = await fetch("http://localhost:3000/api/v1/migrations")
     expect(response.status).toBe(200);
 
     const responseBody = await response.json();
 
     expect(Array.isArray(responseBody)).toBe(true);
+    expect(responseBody.length).toBeGreaterThan(0);
     console.log(responseBody);
+});
+
+test("to envoriment", async () => {
+  expect(process.env.NODE_ENV).toBe("test");
+  console.log(process.env.NODE_ENV);
 });
