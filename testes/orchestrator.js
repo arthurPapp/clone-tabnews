72761a54f03de3c4595db79a8e241da7b0c7179c
Arthur Papp <59942836+arthurPapp@users.noreply.github.com>
2026-08-14 19:45:51 +0000

    ajuste package.json para outra OS

--- testes/orchestrator.js
+++ testes/orchestrator.js
@@ -6,9 +6,12 @@ async function waitForAllServices() {
     await waitForWebServices();
 
     async function waitForWebServices() {
-        return retry(fetchStatusPage)
+        return retry(fetchStatusPage,{
+            retries: 100,
+            maxTimeout: 1000,
+        })
 
-        async function fetchStatusPage() {
+        async function fetchStatusPage(bail) {
             const response = await fetch("http://localhost:3000/api/v1/status")
             const responseBody = await response.json();
         }
